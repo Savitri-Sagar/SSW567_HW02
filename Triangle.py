00b7a2ba9669bcb774da8c9d49ab980c8268187f
kristentan1 <ktan1@stevens.edu>
2018-09-04 19:02:33 -0400

    add modified classification logic

--- Triangle.py
+++ Triangle.py
@@ -9,6 +9,8 @@ The primary goal of this file is to demonstrate a simple python program to class
 @author: rk
 """
 
+# This file was created by me to be able to run the Test file for HW02a.
+
 def classifyTriangle(a,b,c):
     """
     Your correct code goes here...  Fix the faulty logic below until the code passes all of 
@@ -26,32 +28,31 @@ def classifyTriangle(a,b,c):
       
       BEWARE: there may be a bug or two in this code
     """
-
     # require that the input values be >= 0 and <= 200
     if a > 200 or b > 200 or c > 200:
-        return 'InvalidInput'
-        
-    if a <= 0 or b <= b or c <= 0:
+        return "InvalidInput"
+     
+    if a <= 0 or b <= 0 or c <= 0:
         return 'InvalidInput'
     
     # verify that all 3 inputs are integers  
     # Python's "isinstance(object,type) returns True if the object is of the specified type
     if not(isinstance(a,int) and isinstance(b,int) and isinstance(c,int)):
         return 'InvalidInput';
-      
-    # This information was not in the requirements spec but 
-    # is important for correctness
-    # the sum of any two sides must be strictly less than the third side
-    # of the specified shape is not a triangle
-    if (a >= (b - c)) or (b >= (a - c)) or (c >= (a + b)):
+    
+    # This information was not in the requirements spec but is important for correctness
+    # The sum of any two sides must be greater than the third side
+    if ((a+b) < c) or ((a+c) < b) or ((c+b) < a):
         return 'NotATriangle'
-        
-    # now we know that we have a valid triangle 
-    if a == b and b == a:
+    
+    # now we know that we have a valid triangle
+    if a == b and b == c:
         return 'Equilateral'
-    elif ((a * 2) + (b * 2)) == (c * 2):
-        return 'Right'
-    elif (a != b) and  (b != c) and (a != b):
+    elif (((a * 2) + (b * 2)) == (c * 2)) and ((a == b) or (a==c) or (b==c)):
+        return 'Isosceles Right'
+    elif (((a * 2) + (b * 2)) == (c * 2)) and ((a!=b) and (b!=c) and (a!=c)):
+        return 'Scalene Right'
+    elif (a != b) and  (b != c) and (a != c):
         return 'Scalene'
     else:
-        return 'Isoceles'
+        return 'Isosceles'
